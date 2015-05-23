903fd294ebdc98a2e29a91d9248295365837aa13
postalguy <akrim.ismail.cp1@gmail.com>
2015-05-23 15:14:19 +0100

    DPFUser complete and working

--- app/src/main/java/sqlapp/akrimkermi/com/DataParsingAndFetching/DPFEtudiant.java
+++ app/src/main/java/sqlapp/akrimkermi/com/DataParsingAndFetching/DPFEtudiant.java
@@ -1,5 +1,7 @@
 package sqlapp.akrimkermi.com.DataParsingAndFetching;
 
+import android.content.Context;
+
 import sqlapp.akrimkermi.com.Metier.User;
 import sqlapp.akrimkermi.com.Metier.Etudiant;
 import sqlapp.akrimkermi.com.Metier.Groupe;
@@ -7,4 +9,19 @@ import sqlapp.akrimkermi.com.Metier.Groupe;
  * Created by akrim on 23/05/2015.
  */
 public class DPFEtudiant {
+    private Context con;
+    private String link = "http://localhost/GestionDeCours/Ressources/Etudiant.json";// lien temporaire
+    public volatile boolean parsingComplete = true;
+
+
+    public DPFEtudiant() {
+    }
+
+    public DPFEtudiant(String link) {
+        this.link = link;
+    }
+
+    public DPFEtudiant(Context con) {
+        this.con = con;
+    }
 }
